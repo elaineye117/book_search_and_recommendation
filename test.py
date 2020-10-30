fe1661479027371c9463c904f5a285aa59710bf4
elaineye117 <43073716+elaineye117@users.noreply.github.com>
2020-10-30 10:18:33 -0400

    Finished scrappying, all the data is stored inthe BooksData.csv

--- test.py
+++ test.py
@@ -1,15 +1,14 @@
 from bs4 import BeautifulSoup
 import requests
 
-url = 'https://fourminutebooks.com/10-days-to-faster-reading-summary/'
+url = 'https://fourminutebooks.com/book-summaries/'
 
 headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
 result = requests.get(url, headers=headers)
-# print(result.content.decode())
 
-soup = BeautifulSoup(result.content, "html.parser")
+soup = BeautifulSoup(result.text)
 
-div_right = soup.find_all('div', class_='w4pl-inner')
+div_right = soup.find_all('div', class_='w4pl')
 
 # for index, i in enumerate(div_right, 1):
 #     try:
@@ -17,4 +16,4 @@ div_right = soup.find_all('div', class_='w4pl-inner')
 #     except:
 #         name = 'None'
 
-print(soup)
+print(div_right)
