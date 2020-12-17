4b9c60c2760754e581a0e1dee1c0d940bca087cd
elaineye117 <43073716+elaineye117@users.noreply.github.com>
2020-12-16 19:54:41 -0500

    Update scrap.py

--- scrap.py
+++ scrap.py
@@ -42,3 +42,89 @@ summary['response'] = summary['response'].str.replace('1-Sentence-Summary: ', ''
 
 # Rename column name
 summary = summary.rename(columns={0: "response"})
+
+
+# Scrap book name from the website
+
+def get_title(url):
+    link = url
+    result_page = requests.get(link, headers=headers)
+    soup_page = BeautifulSoup(result_page.content, "html.parser")
+    table = soup_page.find('div', class_='entry-content')
+    p = table.find('p')
+    try:     
+        title = p.find('a').get_text()
+    except AttributeError:
+        title = "no title"
+    return title
+
+for link in tqdm(booklisturl):
+    titles.append(get_title(link))
+   
+
+books = books.rename(columns={0: "book"})
+
+
+
+# Obtain category for each book
+def get_cat(url):
+    book_classes = []
+    link = url
+    result_page = requests.get(link, headers=headers)
+    soup_page = BeautifulSoup(result_page.content, "html.parser")
+    table = soup_page.find('div', class_='entry-meta')
+    categories = table.find_all('a', rel="category tag")
+    for i in categories:
+        a = i.get_text()
+        book_class.append(a) 
+    return book_classes
+
+
+class_ = []
+for link in tqdm(booklisturl):
+    class_.append(get_cat(link))
+    
+class_ = []
+for j in class_:
+    j = str(j)
+    classes.append(j)
+ 
+# save class into a datafram and save it into a csv file
+classs = pd.DataFrame(classes)
+
+category = list(df['book_category'])
+classs.to_csv("BooksData3.csv")
+
+# Annotation!!!
+
+df = pd.read_csv("BooksData.csv")
+category = list(df['book_category'])
+
+point = 0
+annotation = []
+for i in tqdm(category):
+    a = 0
+    if 'Happiness' in i:
+        a += 1
+    elif 'Self Improvement' in i:
+        a += 1
+    elif 'Health'in i:
+        a += 1
+    elif 'Motivation' in i:
+        a += 0.5
+    elif 'Career' in i:
+        a += 0.5
+    #4/5 = 0.8, 0.8x3=2.4
+    if a > 2.4:
+        point = 3
+    elif a < 1.6 and a > 0.8:
+        point = 2
+    elif a > 0 and a< 0.8:
+        point = 1
+    else:
+        point = 0
+    annotation.append(point)
+   
+annotation = pd.DataFrame(annotation)
+
+annotation.to_csv('annotation.csv')
